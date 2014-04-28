5cfd233ec576b2c3275d981de1269ae610f2092a
arktisklada <mail@enorganik.com>
2014-04-28 18:35:22 -0400

    adding STDIN user input for login

--- lib/tour.rb
+++ lib/tour.rb
@@ -1,9 +1,28 @@
 require "tour/version"
+require "optparse"
+
 
 module Tour
   class Client
-    def start
-      
+    attr_accessor :username, :password
+
+    def initialize
+      @server = ''
+      @username = ''
+      @password = ''
     end
+
+    def login
+      print "Remote Server Address: "
+      @server = STDIN.gets.chomp
+      print "Username: "
+      @username = STDIN.gets.chomp
+      print "Password: "
+      @password = STDIN.gets.chomp
+    end
+
+    def start
+    end
+
   end
 end
